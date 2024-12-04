35274a726463013ee2a8daa4f5e1814b647b10ed
yallayaswanthsaisurya <159991368+yallayaswanthsaisurya@users.noreply.github.com>
2024-12-04 10:49:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,5 +10,8 @@ terraform {
 provider "azurerm" {
   features {}
   subscription_id = "c00531d6-c3a3-48bf-8a58-2cc719fdca4d"
+  client_id  = "142ce2ac-6f62-4204-9b9f-6f6099727687"
+  client_secret  = "Phs8Q~nu0XKlBuHKs6S-93Rj6cnNnTuQNriM.dcx"
+  tenant_id   = "04de910f-1e14-468a-9411-489fb3141296"
 
 }
